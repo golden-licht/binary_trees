26e6908fd7ad2e09a9da8f2360d25cbb63d07b29
golden-licht <luminoonimul@gmail.com>
2024-04-12 00:04:24 +0300

    Declare variables with C89 standard

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,10 +12,12 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (!tree)
 		return (0);
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
