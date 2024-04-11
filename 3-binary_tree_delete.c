f0853689c13d15250438fb19a6d35abd92efb944
golden-licht <luminoonimul@gmail.com>
2024-04-11 10:46:59 +0300

    Delete an entire binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Delete an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
